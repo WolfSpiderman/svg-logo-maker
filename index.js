9d07271292cb204ef65d5e5afe75b7a375c6ee64
WolfSpiderman <wolfspiderman@yahoo.com>
2023-04-14 05:30:55 -0500

    added validations for text and colors

--- index.js
+++ index.js
@@ -1,16 +1,44 @@
 const inquirer = require("inquirer");
+const colorString = require('color-string');
 
 // Questions to ask user
 const questions = [
     {
         name: "text",
         message: "Insert up to three characters to be the text content of your symbol.",
-        type: "input"
+        type: "input",
+        validate: function (input) {
+            if (/^[a-zA-Z0-9]+$/.test(input) && input.length <= 3 && input.length >= 1) {
+                return true;
+            } else {
+                return "Please enter a valid input. Can only contain letter and numbers and must be no more than 3 characters."
+            }
+        },
+        filter: function(input) {
+            // Convert input to lowercase
+            return input.toUpperCase();
+          }
     },
     {
         name: "color",
         message: "What color would you like the text to be? Enter the name of a common color, or a hex-code.",
-        type: "input"
+        type: "input",
+        validate: function(input) {
+          if (/^#?([a-fA-F0-9]{6}|[a-fA-F0-9]{3})$/.test(input)) {
+            return true;
+          } else if (/^[a-zA-Z]+$/.test(input)) {
+            const colorObject = colorString.get(input);
+            if (colorObject !== null) {
+                return true;
+            }
+          } else {
+            return "Please enter a valid color name or hex-code.";
+          }
+        },
+        filter: function(input) {
+          // Convert input to lowercase
+          return input.toLowerCase();
+        }
     },
     {
         name: "shape",
@@ -21,12 +49,29 @@ const questions = [
     {
         name: "bgColor",
         message: "What color would you like the symbol's background, the shape, to be? Enter the name of a common color, or a hex-code.",
-        type: "input"
+        type: "input",
+        validate: function(input) {
+          if (/^[a-zA-Z]+$/.test(input) || /^#?([a-fA-F0-9]{6}|[a-fA-F0-9]{3})$/.test(input)) {
+            const colorObject = colorString.get(input);
+            if (colorObject !== null) {
+                return true;
+            } else {
+                return "Please enter a valid color name or hex-code.";
+            }
+          }
+        },
+        filter: function(input) {
+          // Convert input to lowercase
+          return input.toLowerCase();
+        }
+    },
+    {
+        name: "border",
+        message: "Would you like to add a border the same color as your text to your logo?",
+        type: "confirm"
     }
 ]
 
 
 
-inquirer.prompt(questions).then(
-    
-)
+inquirer.prompt(questions).then(data => console.log(data))
